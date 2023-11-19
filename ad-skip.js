148b94b312844f3fb08cb73082bead9f18d52936
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2023-11-19 16:22:19 +0800

    Update ad-skip.js

--- ad-skip.js
+++ ad-skip.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         自动跳过YouTube广告
 // @namespace    youtube
-// @version      1.0
+// @version      1.1
 // @description  在YouTube网页上自动跳过广告
-// @author       Joey Gambler
+// @author       飞机上的长电视(@realtvop)
 // @match        *://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
@@ -12,13 +12,17 @@
     'use strict';
 
     function skipAd() {
-        var skipButton = document.querySelector('.ytp-ad-text.ytp-ad-skip-button-text');
+        const skipButton = document.querySelector('.ytp-ad-text.ytp-ad-skip-button-text');
         if (skipButton) {
             skipButton.click();
-            console.log("Click button");
+            console.log("Skip button clicked.");
+        } else if (document.querySelector(".ytp-ad-text")) {
+            const videoElement = document.querySelector("video.video-stream.html5-main-video");
+            videoElement.currentTime = videoElement.duration;
+            console.log("Ad skiped.");
         }
     }
 
     // 设置检测时间间隔
-    var timer = setInterval(skipAd, 1000); // 1000毫秒 = 1秒
+    var timer = setInterval(skipAd, 10);
 })();
